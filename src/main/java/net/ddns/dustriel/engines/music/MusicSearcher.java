afc388fd20686f7773810cf6c2aa446486c82848
Ricardo Espirito Santo <ricardo.espiritosanto@trumin.com>
2016-09-10 18:39:09 +0100

    Completing a bit more the interfaces and documentation

--- src/main/java/net/ddns/dustriel/engines/music/MusicSearcher.java
+++
@@ -1,8 +0,0 @@
-package net.ddns.dustriel.engines.music;
-
-/**
- * The contract to which 'music' APIs have to adhere to be usable by our Gop Gup system.
- */
-public interface MusicSearcher {
-
-}
--- src/main/java/net/ddns/dustriel/model/music/MusicSearchResult.java
+++ src/main/java/net/ddns/dustriel/model/music/MusicSearchResult.java
@@ -1,8 +1,44 @@
 package net.ddns.dustriel.model.music;
 
 /**
- * Created by ricardo on 10/09/2016.
+ * The result of searching for a music album will fill in the details in this pojo. This class aims to hold basic album
+ * data.
  */
-public interface MusicSearchResult {
+public class MusicSearchResult {
 
+    private String band;
+
+    private String releaseDate;
+
+    private String name;
+
+    public MusicSearchResult(String band, String releaseDate, String name) {
+        this.band = band;
+        this.releaseDate = releaseDate;
+        this.name = name;
+    }
+
+    public String getBand() {
+        return band;
+    }
+
+    public void setBand(String band) {
+        this.band = band;
+    }
+
+    public String getReleaseDate() {
+        return releaseDate;
+    }
+
+    public void setReleaseDate(String releaseDate) {
+        this.releaseDate = releaseDate;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
