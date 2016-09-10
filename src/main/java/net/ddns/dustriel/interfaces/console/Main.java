269b8fedc14ec31de9b1376382035fc7a7c28de6
Ricardo Espirito Santo <ricardo.espiritosanto@trumin.com>
2016-09-10 18:58:06 +0100

    And the test is fixed. Go TDD

--- src/main/java/net/ddns/dustriel/interfaces/console/Main.java
+++ src/main/java/net/ddns/dustriel/interfaces/console/Main.java
@@ -1,5 +1,7 @@
 package net.ddns.dustriel.interfaces.console;
 
+import java.util.Arrays;
+
 /**
  * Main, executable, entry point to start our gop gup system as a console application
  */
@@ -9,9 +11,17 @@ public class Main {
         // Hiding the private constructor
     }
 
-    public static void main(String ... arguments) {
+    public static void main(String... arguments) {
 
+        assertArgumentsAreValid(arguments);
         System.out.println("Using " + arguments[0] + " as arguments");
+    }
+
+    private static void assertArgumentsAreValid(String[] arguments) {
+        if (Arrays.stream(arguments).filter(argument -> argument.equalsIgnoreCase("API")).count() != 1) {
+            throw new InvalidArgumentsException(
+                    "'API' is a required argument. Please specify the API to be used when running your query");
+        }
     }
 
 }
